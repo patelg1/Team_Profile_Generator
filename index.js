23bc5835c701766642756defd5cbee12500ba93c
Gopal Patel <patelgopalk1@gmail.com>
2021-03-03 17:39:50 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+let team = [];
+
