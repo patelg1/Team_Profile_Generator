36b1fe4f80ab3cfe30cf4c6950350483d52c48d9
Gopal Patel <patelgopalk1@gmail.com>
2021-03-04 16:58:38 -0500

    Added tests for Intern

---
+++ tests/Intern.test.js
@@ -0,0 +1,23 @@
+const Intern = require('../lib/Intern');
+
+describe('getSchool', () => {
+    it('Sets the school name using constuctor', () => {
+        const school = 'UPENN';
+        const employee = new Intern('Test', 1, 'test@email.com', school);
+        expect(employee.school).toBe('UPENN');
+    });
+    
+    it('Returns the school name using getOfficeNumber()', () => {
+        const school = 'UPENN';
+        const employee = new Intern('Test', 1, 'test#email.com', 'UPENN');
+        expect(employee.getSchool()).toBe(school);
+    });
+});
+
+describe('getRole', () => {
+    it('Returns role as Intern', () => {
+        const role = 'Intern'
+        const employee = new Intern('Test', 1, 'test@email.com', 'UPENN');
+        expect(employee.getRole).toBe(role);
+    }); 
+});
