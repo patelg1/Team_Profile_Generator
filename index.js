79f095cb209b3fa13f95cd86c1e0fa62d5cdb51a
Gopal Patel <patelgopalk1@gmail.com>
2021-03-05 15:45:21 -0500

    added function to create manager, engineer, intern

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function askQuestions(){
     ]).then((answers) => {
         switch (answers.employee){
             case ('Engineer'):
-                //createEngineer();
+                createEngineer();
                 break;
             case ('Intern'):
-                //createIntern();
+                createIntern();
                 break;
             default:
                 //renderHTML();
@@ -33,7 +33,7 @@ function createMananger(){
     inquirer.prompt([
         {
             type: 'input',
-            message: 'Enter the manager name:',
+            message: "Enter the manager's name:",
             name: 'managerName',
             validate: function(input){
                 if (!isNaN(parseFloat(input))){
@@ -45,22 +45,94 @@ function createMananger(){
         },
         {
             type: 'input',
-            message: 'Enter the manager id:',
+            message: "Enter the manager's id:",
             name: 'managerId'
         },
         {
             type: 'input',
-            message: 'Enter the manager email adress:',
+            message: "Enter the manager's  email adress:",
             name: 'managerEmail'
         },
         {
             type: 'input',
-            message: 'Enter the manager office number:',
+            message: "Enter the manager's office number:",
             name: 'managerOffice'
         },
     ]).then((answers) => {
         const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice);
         teamArray.push(manager)
+        askQuestions();
+    })
+}
+
+function createEngineer(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter the engineer's name:",
+            name: 'engineerName',
+            validate: function(input){
+                if (!isNaN(parseFloat(input))){
+                    return false;
+                }else{
+                    return true
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's id:",
+            name: 'engineerId'
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's email adress:",
+            name: 'engineerEmail'
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's Github username:",
+            name: 'github'
+        },
+    ]).then((answers) => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.github);
+        teamArray.push(engineer);
+        askQuestions();
+    })
+}
+
+function createIntern(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter the intern's name:",
+            name: 'internName',
+            validate: function(input){
+                if (!isNaN(parseFloat(input))){
+                    return false;
+                }else{
+                    return true
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "Enter the intern's id:",
+            name: 'internId'
+        },
+        {
+            type: 'input',
+            message: "Enter the intern's email adress:",
+            name: 'internEmail'
+        },
+        {
+            type: 'input',
+            message: "Enter the intern's school:",
+            name: 'school'
+        },
+    ]).then((answers) => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.school);
+        teamArray.push(intern);
         askQuestions();
     })
 }
