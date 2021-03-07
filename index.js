d40b869d03ab4d75b891522a432466be9e773636
Gopal Patel <patelgopalk1@gmail.com>
2021-03-07 11:07:52 -0500

    added function to create html

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const Manager = require('./lib/Manager');
 
 let teamArray = [];
 
+function startApp(){
+    inquirer.prompt([
+        {
+            message: 'Welcome to the Team Generator. Please write your team name:',
+            name: 'teamName'
+        }
+    ])
+    .then(function(data){
+        const teamName = data.teamName;
+        teamArray.push(teamName);
+        createMananger();
+    })
+}
+
 function askQuestions(){
     inquirer.prompt([
         {
@@ -23,7 +37,7 @@ function askQuestions(){
                 createIntern();
                 break;
             default:
-                //renderHTML();
+                renderHTML();
         }
 
     });
@@ -60,7 +74,8 @@ function createMananger(){
         },
     ]).then((answers) => {
         const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice);
-        teamArray.push(manager)
+        const role = manager.getRole();
+        teamArray.push(manager, role)
         askQuestions();
     })
 }
@@ -96,7 +111,8 @@ function createEngineer(){
         },
     ]).then((answers) => {
         const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.github);
-        teamArray.push(engineer);
+        const role = engineer.getRole();
+        teamArray.push(engineer, role);
         askQuestions();
     })
 }
@@ -132,9 +148,110 @@ function createIntern(){
         },
     ]).then((answers) => {
         const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.school);
-        teamArray.push(intern);
+        let role = intern.getRole();
+        teamArray.push(intern, role);
         askQuestions();
     })
 }
 
-createMananger()
+function renderHTML(){
+    console.log("//////You have done it!!!//////")
+    const htmlArray = [];
+    const htmlBegin = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <title>${teamArray[0]}</title>      
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+</head>
+<body>
+    <header class="bg-danger">
+        <h1 class="text-center mb-5 p-5 text-white">${teamArray[0]}</h1>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                
+    `
+    htmlArray.push(htmlBegin);
+    for (let i = 1; i < teamArray.length; i++) {
+        let object = `
+                
+                    
+        `
+        if(teamArray[i].officeNumber) {
+            object += `
+            <div class="card m-2 col-3 border-0">
+                <div class="card-header bg-primary text-white">
+                <h3 class="card-title">${teamArray[i].name}</h3>
+                <h4 class="card-title"><i class="fas fa-mug-hot"></i> ${teamArray[i].getRole()}</h4>
+                </div>
+                <div class="card-body bg-light">
+                    <ul class="list-group py-3">
+                        <li class="list-group-item">ID: ${teamArray[i].id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</li></a>
+                        <li class="list-group-item">Office number: ${teamArray[i].officeNumber}</li>
+                    </ul>
+                </div>
+            </div>            
+            `
+        }
+        if(teamArray[i].github) {
+            object += `
+            <div class="card m-2 col-3 border-0">
+                <div class="card-header bg-primary text-white">
+                <h3 class="card-title">${teamArray[i].name}</h3>
+                <h4 class="card-title"><i class="fas fa-glasses"></i> ${teamArray[i].getRole()}</h4>
+                </div>
+                <div class="card-body bg-light">
+                    <ul class="list-group py-3">
+                        <li class="list-group-item">ID: ${teamArray[i].id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</li></a>
+                        <li class="list-group-item">GitHub: <a href="https://github.com/${teamArray[i].github}">${teamArray[i].github}</li></a>
+                    </ul>
+                </div>
+            </div>                
+            `
+        }
+        if(teamArray[i].school){
+            object += `
+            <div class="card m-2 col-3 border-0">
+                <div class="card-header bg-primary text-white">
+                <h3 class="card-title">${teamArray[i].name}</h3>
+                <h4 class="card-title"><i class="fas fa-user-graduate"></i> ${teamArray[i].getRole()}</h4>
+                </div>
+                <div class="card-body bg-light">
+                    <ul class="list-group py-3">
+                        <li class="list-group-item">ID: ${teamArray[i].id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</li></a>
+                        <li class="list-group-item">School: ${teamArray[i].school}</li>
+                    </ul>
+                </div>
+            </div>            
+            `
+        } 
+        object += `
+                    
+        ` 
+        htmlArray.push(object);   
+    }
+
+    
+    const htmlEnd = `
+            </div>
+        </div>
+    </div>
+</body>
+</html> 
+    `
+    htmlArray.push(htmlEnd);
+    fs.writeFile(`./dist/${teamArray[0]}.html`, htmlArray.join(''), function(err){
+        
+    });
+}
+
+startApp();
